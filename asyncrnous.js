0a6a02d77b4877e907b42289e92b8b011b5ec117
Aamir Sohail <128887071+aamircheema@users.noreply.github.com>
2023-07-09 00:31:18 -0700

    Update asyncrnous.js
    Doing same task with Promises

--- asyncrnous.js
+++ asyncrnous.js
@@ -1,4 +1,4 @@
-// Callback example
+<--  Callback example  -->
 let order = (call_production) =>{
 
 console.log("Order placed. Please call production")
@@ -16,7 +16,10 @@ order(production);
 // output
 //Production has started
 //strawberry was selected
-// Example Callback hell 
+<-- End of call back example -->
+
+
+<-- Example Callback hell --> 
 let stocks = {
     Fruits: ['strawberry', 'grapes', 'banana', 'apple'],
     liquid: ["water", "ice"],
@@ -63,3 +66,91 @@ order(1, production);
 // chocolate as toppings
 // Serve Ice cream
 // Production has started
+<-- End of Callback hell example -->
+
+<-- Doing same task using promises -->
+  let stocks = {
+    Fruits: ['strawberry', 'grapes', 'banana', 'apple'],
+    liquid: ["water", "ice"],
+    holder: ["cone", "cup", "stick"],
+    toppings: ["chocolate", "peanuts"],
+}
+
+let is_shop_open = true;
+
+let order = (time, work) => {
+    return new Promise((resolve, reject) => {
+        if (is_shop_open) {
+            setTimeout(() => {
+                resolve(work())
+            }, time)
+        } else {
+            reject(console.log('Shop is closed'))
+        }
+    })
+}
+order(2000, () => {
+    console.log(`${stocks.Fruits[1]} was seleted`);
+})
+
+    .then(() => {
+        return order(0000, () => {
+            console.log(`Production has started`)
+        });
+    })
+
+    .then(() => {
+        return order(2000, () => {
+            console.log(`Fruit has been chopped`)
+        })
+    })
+
+    .then(() => {
+        return order(1000, () => {
+            console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} added`)
+        })
+    })
+
+    .then(() => {
+        return order(1000, () => {
+            console.log(`Start the machine`)
+        })
+    })
+
+    .then(() => {
+        return order(2000, () => console.log(`Ice cream placed on ${stocks.holder[0]}`))
+    })
+
+    .then(() => {
+        return order(3000, () => console.log(`Selected ${stocks.toppings[0]} as topping`))
+    })
+
+
+    .then(() => {
+        return order(2000, () => console.log("Serve Ice Cream"))
+    })
+
+    .catch(() => {
+        console.log(`Customer left`)
+    })
+
+    .finally(() => {
+        console.log(`End of the day`)
+    })
+
+// if is_shop_open = true then Output will be
+// grapes was seleted
+// Production has started
+// Fruit has been chopped
+// water and ice added
+// Start the machine
+// Ice cream placed on cone
+// Selected chocolate as topping
+// Serve Ice Cream
+// End of the day
+
+// // if is_shop_open = false then Output will be
+// Shop is closed
+// Customer left
+// End of the day
+
