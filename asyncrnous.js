bdc16c1e71e0091b5a60d5bad6abeec1d5def6ea
Aamir Sohail <128887071+aamircheema@users.noreply.github.com>
2023-07-09 02:03:29 -0700

    Update asyncrnous.js
    Doing same task with Async/await function

--- asyncrnous.js
+++ asyncrnous.js
@@ -154,3 +154,57 @@ order(2000, () => {
 // Customer left
 // End of the day
 
+
+
+<-- Doing Same task with async function -->
+  // // Callback function is a function that is passed to another function as a argument
+// // Callback functions are used to handle asyncrnous behaviour of javascript
+
+let stocks = {
+    Fruits: ['strawberry', 'grapes', 'banana', 'apple'],
+    liquid: ["water", "ice"],
+    holder: ["cone", "cup", "stick"],
+    toppings: ["chocolate", "peanuts"],
+}
+
+let is_shop_open = true;
+
+let time = (ms) => {
+    return new Promise((resolve, reject) => {
+        if (is_shop_open) {
+            setTimeout(resolve, ms)
+        } else {
+            reject(console.log("Shop is closed"))
+        }
+    })
+};
+
+async function kitchen() {
+    try {
+        await time(2000)
+        console.log(`${stocks.Fruits[2]} was selected`)
+        await time(0000)
+        console.log(`Production has been started`)
+        await time(2000)
+        console.log(`Fruit was chopped`)
+        await time(1000)
+        console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} is added`)
+        await time(1000)
+        console.log(`Machine was started`)
+        await time(2000)
+        console.log(`Ice cream was placed on ${stocks.holder[1]}`)
+        await time(3000)
+        console.log(`${stocks.toppings[0]} added as topping`)
+        await time(2000)
+        console.log(`Ice cream is served`)
+    }
+
+    catch (error) {
+        console.log('Customer left')
+    }
+    finally {
+        console.log(`Shop Closed`)
+    }
+};
+kitchen();
+  
