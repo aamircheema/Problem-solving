a81b62dda1277a6c51d5e4277c18811547b4bad7
Aamir Sohail <128887071+aamircheema@users.noreply.github.com>
2023-07-08 22:43:42 -0700

    Update and rename callback.js to asyncrnous.js
    adding an example of callback hell

---
+++ asyncrnous.js
@@ -0,0 +1,56 @@
+//N Callback example
+let order = (call_production) =>{
+
+console.log("Order placed. Please call production")
+
+// function 👇 is being called 
+  call_production();
+};
+
+let production = () =>{
+
+console.log("Production has started")
+
+};
+order(production);
+// output
+//Production has started
+//strawberry was selected
+// Example Callback hell 
+let stocks = {
+    Fruits: ['strawberry', 'grapes', 'banana', 'apple'],
+    liquid: ["water", "ice"],
+    holder: ["cone", "cup", "stick"],
+    toppings: ["chocolate", "peanuts"],
+}
+const order = (fruit_name, callProdcution) => {
+    setTimeout(() => {
+        console.log(`${stocks.Fruits[fruit_name]} was selected`)
+        callProdcution();
+        setTimeout(() => {
+            console.log(`The fruit has been chopped`)
+        })
+        setTimeout(() => {
+            console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} added.`)
+        })
+        setTimeout(() => {
+            console.log(`Start the machine`)
+        })
+        setTimeout(() => {
+            console.log(`Ice Cream was placed on ${stocks.holder[2]}`)
+        })
+        setTimeout(() => {
+            console.log(`${stocks.toppings[0]} as toppings`)
+        })
+        setTimeout(() => {
+            console.log(`Serve Ice cream`)
+        })
+    }, 2000)
+};
+const production = () => {
+    setTimeout(() => {
+        console.log(`Production has started`)
+    }, 1000)
+};
+order(1, production);
+
