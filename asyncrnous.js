0e9a934a2e5e20979530c9b793be83305e68ba6a
Aamir Sohail <128887071+aamircheema@users.noreply.github.com>
2023-07-08 22:54:13 -0700

    Update asyncrnous.js

--- asyncrnous.js
+++ asyncrnous.js
@@ -27,30 +27,31 @@ const order = (fruit_name, callProdcution) => {
     setTimeout(() => {
         console.log(`${stocks.Fruits[fruit_name]} was selected`)
         callProdcution();
-        setTimeout(() => {
-            console.log(`The fruit has been chopped`)
-        })
-        setTimeout(() => {
-            console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} added.`)
-        })
-        setTimeout(() => {
-            console.log(`Start the machine`)
-        })
-        setTimeout(() => {
-            console.log(`Ice Cream was placed on ${stocks.holder[2]}`)
-        })
-        setTimeout(() => {
-            console.log(`${stocks.toppings[0]} as toppings`)
-        })
-        setTimeout(() => {
-            console.log(`Serve Ice cream`)
-        })
     }, 2000)
 };
-const production = () => {
+let production = () => {
     setTimeout(() => {
-        console.log(`Production has started`)
-    }, 1000)
+        console.log("Production has started")
+        setTimeout(() => {
+            console.log("The fruit has been chopped")
+            setTimeout(() => {
+                console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} Added`)
+                setTimeout(() => {
+                    console.log("Start the machine")
+                    setTimeout(() => {
+                        console.log(`Ice cream placed on ${stocks.holder[1]}`)
+                        setTimeout(() => {
+                            console.log(`${stocks.toppings[0]} as toppings`)
+                            setTimeout(() => {
+                                console.log("Serve Ice cream")
+                            }, 2000)
+                        }, 3000)
+                    }, 2000)
+                }, 1000)
+            }, 1000)
+        }, 2000)
+    },0000)
+
 };
 order(1, production);
 //Output
