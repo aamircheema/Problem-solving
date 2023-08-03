59bcd7c2bb8970aeba62c2f2b209806aefd96f15
Aamir Sohail <128887071+aamircheema@users.noreply.github.com>
2023-08-03 10:26:11 -0800

    Update closure.js

--- closure.js
+++ closure.js
@@ -1,7 +1,7 @@
 //Closure is a function that retains access to variables from its outer
 //scope even after the outer function has finished executing
 function outerFunction() {
-    var outerVariable = 'I am outer';
+    var outerVariable = 'Outer value';
     function innerFunction() {
         var innerVariable = 'I am inner';
         // Accesses both the outerVariable and innerVariable from its lexical scope
